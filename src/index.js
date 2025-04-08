24869509b0917f4d78cc5dfafc61ee86ae0d64fa
Jowairia <juwairiapathan5@gmail.com>
2025-04-08 12:29:42 -0400

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 require("dotenv").config();
 
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errors");
 const crapRouter = require("./routers/crap");
 const { connect } = require("./models/db");
 const authRouter = require("./routers/auth");
-require('./util/passport');
+require("./util/passport");
 
 connect();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: (process.env.CORS_ALLOWLIST || "").split(","),
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -17,7 +25,7 @@ app.use("/api/craps", crapRouter);
 
 app.use(errorHandler);
 
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, (err) => {
